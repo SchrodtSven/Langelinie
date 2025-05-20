c51f14c29dad66b767df4a8a70827a02a98b23f1
Sven Schrodt <sven@schrodt.club>
2025-05-20 15:27:03 +0200

    Cleaning bot ...

--- dox_ext/m.goldt.py
+++
@@ -1,6 +0,0 @@
-txt = """Im Postscriptum meiner vorletzten Kolumne bemerkte ich mit der lakonischen 
-Beiläufigkeit, die uns waschechten Melancholikern eigen ist, dass ich mich mit 
-dem Gedanken trage, einen Artikel über ungeliebtes Obst, insbesondere über Quitten, 
-zu schreiben"""
-items = txt.split()
-print(len(items))
